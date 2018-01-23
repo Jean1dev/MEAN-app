610d31205bfc5a0fd89450b075e181895be27831
Jean <jeanlucafp@gmail.com>
2018-01-22 22:49:50 -0200

    ,,,

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,3 +3,6 @@ const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost:27017/db_finance')
 
 mongoose.Error.messages.general.required = 'O atributo  é necessario'
+mongoose.Error.messages.Number.min = 'O valor inserido não é permitido'
+mongoose.Error.messages.Number.max = 'O valor inserido não é permitido'
+mongoose.Error.messages.String.enum = 'Valor invalido'
