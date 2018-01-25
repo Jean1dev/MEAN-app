efa71cfd77a0fd7009356de5807fdb5b145518d3
Jean <jeanlucafp@gmail.com>
2018-01-24 22:06:38 -0200

    adcionado front

--- backend/api/billingcycle/billingCycleService.js
+++ backend/api/billingcycle/billingCycleService.js
@@ -4,6 +4,25 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+BillingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
+
+function sendErrorsOrNext(req, res, next){
+  const bundle = res.locals.bundle
+
+  if(bundle.errors){
+    var errors = parseErrors(bundle.errors)
+    res.status(500).json({errors})
+  }else{
+    next()
+  }
+}
+
+function parseErrors(nodeRestFulErrors){
+  const errors = []
+  _.forIn(nodeRestFulErrors, error => errors.push(error.message))
+  return errors
+}
+
 BillingCycle.route('count', function(req, res, next) {
   BillingCycle.count(function(error, value) {
     if(error) {
--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 
 module.exports = mongoose.connect('mongodb://localhost:27017/db_finance')
 
-mongoose.Error.messages.general.required = 'O atributo  é necessario'
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'."
+/*mongoose.Error.messages.general.required = 'O atributo  é necessario'
 mongoose.Error.messages.Number.min = 'O valor inserido não é permitido'
 mongoose.Error.messages.Number.max = 'O valor inserido não é permitido'
-mongoose.Error.messages.String.enum = 'Valor invalido'
+mongoose.Error.messages.String.enum = 'Valor invalido'*/
