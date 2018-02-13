1ff99aae76df5b05d48ff952b044a6c8df486703
Jean <jeanlucafp@gmail.com>
2018-02-13 14:22:36 -0200

    implementando autenticação

--- angular1/gulptasks/server.js
+++ angular1/gulptasks/server.js
@@ -12,7 +12,7 @@ gulp.task('watch', () => {
 gulp.task('server', ['watch'], () => {
     return gulp.src('public').pipe(webserver({
         livereload: true,
-        port: 802,
+        port: 80,
         open: true
     }))
 })
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,19 +1,38 @@
 const express = require('express')
+const auth = require('./auth')
 
-module.exports = function(server) {
+module.exports = function (server) {
 
-  // API Routes
-  const router = express.Router()
-  server.use('/api', router)
+  // Rotas abertas
+  const openApi = express.Router()
+  server.use('/oapi', openApi)
+  const AuthService = require('../api/Users/authService')
+  openApi.post('/login', AuthService.login)
+  openApi.post('/signup', AuthService.signup)
+  openApi.post('/validateToken', AuthService.validateToken)
+
+  // Rotas protegidas por Token JWT
+
+  const protectedApi = express.Router()
+  server.use('/api', protectedApi)
+  protectedApi.use(auth)
+
+  // API Routes PUBLIC - em caso de der pau em tudo
+  // const router = express.Router()
+  // server.use('/api', router)
 
   // rotas da API
   const billingCycleService = require('../api/billingCycle/billingCycleService')
-  billingCycleService.register(router, '/billingCycles')
+  // billingCycleService.register(router, '/billingCycles') USAR SE FOR PUBLIC
+  billingCycleService.register(protectedApi, '/billingCycles')
+
 
   const billingSummaryService = require('../api/billingSummary/billingSummaryService')
-  router.route('/billingSummary').get(billingSummaryService.getSummary)
+  //router.route('/billingSummary').get(billingSummaryService.getSummary) USAR SE FOR PUBLIC
+  protectedApi.route('/billingSummary').get(billingSummaryService.getSummary)
 
-  const UserService = require('../api/Users/UserService')
-  UserService.register(router, '/users')
+  // Rotas do usuario usando outra api
+  //const UserService = require('../api/Users/UserService')
+  //UserService.register(router, '/users')
 
 }
